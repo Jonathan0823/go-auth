767930cd736ac0f1ee7d414d131b081e22f8cfe2
Guinn <eguinjonathan08@gmail.com>
2024-12-15 15:09:50 +0700

    Simplify error message for existing user check in Register method

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -30,7 +30,7 @@ func (s *authservice) Register(user models.User) error {
 	isUserExist := s.repo.IsUserExists(user.Email)
 
 	if isUserExist {
-		return fmt.Errorf("user with email %s already exists", user.Email)
+		return fmt.Errorf("Email is already exists")
 	}
 
 	return s.repo.Register(user)
